ee952b1afd8afe242ece5cd3a11065b73bee4739
Iscle <albertiscle9@gmail.com>
2021-10-26 15:47:48 +0200

    Fix compiler warnings

--- SpaceCadetPinball/TComponentGroup.cpp
+++ SpaceCadetPinball/TComponentGroup.cpp
@@ -45,7 +45,7 @@ int TComponentGroup::Message(int code, float value)
 		if (value > 0.0f)
 			this->Timer = timer::set(value, this, NotifyTimerExpired);
 	}
-	else if (code <= 1007 || code > 1011 && code != 1020 && code != 1022)
+	else if (code <= 1007 || (code > 1011 && code != 1020 && code != 1022))
 	{
 		for (auto component : List)
 		{
