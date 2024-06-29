e0d62792e8e8c190a5452254d0b72294b8f92cd2
Orazio <22700499+orazioedoardo@users.noreply.github.com>
2024-06-29 16:02:04 +0200

    Update SDL and SDL_mixer on macOS

--- build-mac-app.sh
+++ build-mac-app.sh
@@ -6,13 +6,13 @@ mkdir -p Libs
 
 cd Libs
 
-sdl_version='2.28.1'
+sdl_version='2.30.4'
 sdl_filename="SDL2-$sdl_version.dmg"
 sdl_url="https://github.com/libsdl-org/SDL/releases/download/release-$sdl_version/$sdl_filename"
 
-sdl_mixer_version='2.7.0'
+sdl_mixer_version='2.8.0'
 sdl_mixer_filename="SDL2_mixer-$sdl_mixer_version.dmg"
-sdl_mixer_url="https://www.libsdl.org/tmp/$sdl_mixer_filename"
+sdl_mixer_url="https://github.com/libsdl-org/SDL_mixer/releases/download/release-$sdl_mixer_version/$sdl_mixer_filename"
 
 mount_point="$(mktemp -d)"
 
@@ -20,7 +20,7 @@ if [ ! -f "$sdl_filename" ]; then
 	curl -sSf -L -O "$sdl_url"
 fi
 
-echo "2f936225c10a402cab07055e6f75de76f991945c37feb0cf6af633a96d2fb28c  $sdl_filename" | shasum -a 256 -c
+echo "2bf2cb8f6b44d584b14e8d4ca7437080d1d968fe3962303be27217b336b82249  $sdl_filename" | shasum -a 256 -c
 hdiutil attach "$sdl_filename" -mountpoint "$mount_point" -quiet
 cp -a "$mount_point/SDL2.framework" .
 hdiutil detach "$mount_point"
@@ -29,7 +29,7 @@ if [ ! -f "$sdl_mixer_filename" ]; then
 	curl -sSf -L -O "$sdl_mixer_url"
 fi
 
-echo "f394c714c8aefdcae0ff9d6eefeb5d42f28e56ed09fcaebb796cb672ca11279d  $sdl_mixer_filename" | shasum -a 256 -c
+echo "aea973d78f2949b0b2404379dfe775ac367c69485c1d25a5c59f109797f18adf  $sdl_mixer_filename" | shasum -a 256 -c
 hdiutil attach "$sdl_mixer_filename" -mountpoint "$mount_point" -quiet
 cp -a "$mount_point/SDL2_mixer.framework" .
 hdiutil detach "$mount_point"
